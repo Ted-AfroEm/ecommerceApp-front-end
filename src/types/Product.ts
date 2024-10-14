5761a634fb5473e999438f390d8d2a8773371d99
Tedros Kahsu <tedkahcom@gmail.com>
2024-10-14 16:56:11 +0300

    Add product type

---
+++ src/types/Product.ts
@@ -0,0 +1,13 @@
+export interface Product {
+    bestseller: boolean;
+    category: string;
+    date: number;
+    description: string;
+    image: string[];
+    name: string;
+    price: number;
+    sizes: string[];
+    subCategory: string;
+    _id: string;
+  }
+  
